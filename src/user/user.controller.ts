49c44561f23b7e421598566caf2acecbbc3a9503
OnoprienkoArtem <o.artem.e@gmail.com>
2021-09-24 16:37:14 +0300

    add method create to user service and resolve cyrcular issues

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,12 +2,12 @@ import { Controller, Get } from '@nestjs/common';
 import { User } from './models/user.entity';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
   async all(): Promise<User[]> {
-    return await this.userService.all();
+    return this.userService.all();
   }
 }
