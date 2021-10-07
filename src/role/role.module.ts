e61894cb9b0fdba7cec485a6940ed66e6d9375cc
OnoprienkoArtem <o.artem.e@gmail.com>
2021-10-07 22:33:04 +0300

    define role model

--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoleController } from './role.controller';
 import { RoleService } from './role.service';
+import { Role } from './role/role.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Role])],
   controllers: [RoleController],
   providers: [RoleService],
 })
