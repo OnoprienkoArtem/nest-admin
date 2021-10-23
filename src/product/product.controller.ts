702826886fd10fb9c2493c3ad97d8f315dbd910a
OnoprienkoArtem <o.artem.e@gmail.com>
2021-10-23 22:32:37 +0300

    work with product controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/auth.guard';
+import { ProductService } from './product.service';
 
-@Controller('product')
-export class ProductController {}
+@UseGuards(AuthGuard)
+@Controller('products')
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @Get()
+  async all(@Query('page') page = 1) {
+    return this.productService.paginate(page);
+  }
+}
