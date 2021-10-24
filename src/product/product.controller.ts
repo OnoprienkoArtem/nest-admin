2da6e2a17a5a56bd9f6e605e46c9405a3837b64a
OnoprienkoArtem <o.artem.e@gmail.com>
2021-10-24 22:59:51 +0300

    add method update, delete to product controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
-import { ProductCreateDto } from "./models/product-create.dto";
+import { ProductCreateDto } from './models/product-create.dto';
+import { ProductUpdateDto } from './models/product-update.dto';
 import { ProductService } from './product.service';
 
 @UseGuards(AuthGuard)
@@ -16,5 +27,22 @@ export class ProductController {
   @Post()
   async create(@Body() body: ProductCreateDto) {
     return this.productService.create(body);
+  }
+
+  @Get(':id')
+  async get(@Param('id') id: number) {
+    return this.productService.findOne({ id });
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() body: ProductUpdateDto) {
+    await this.productService.update(id, body);
+
+    return this.productService.findOne({ id });
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    return this.productService.delete(id);
   }
 }
