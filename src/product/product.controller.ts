68da609ec8bd164049b91f598af93a16f4a65f82
OnoprienkoArtem <o.artem.e@gmail.com>
2021-10-24 22:33:00 +0300

    add create method and product create dto

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from "@nestjs/common";
 import { AuthGuard } from '../auth/auth.guard';
+import { ProductCreateDto } from "./models/product-create.dto";
 import { ProductService } from './product.service';
 
 @UseGuards(AuthGuard)
@@ -10,5 +11,10 @@ export class ProductController {
   @Get()
   async all(@Query('page') page = 1) {
     return this.productService.paginate(page);
+  }
+
+  @Post()
+  async create(@Body() body: ProductCreateDto) {
+    return this.productService.create(body);
   }
 }
