e52868a1dd9c8811af7be5ee2e6ed96ca4b5dbaa
OnoprienkoArtem <o.artem.e@gmail.com>
2021-10-27 22:57:07 +0300

    add uploads folder for image files

--- src/product/upload.controller.ts
+++ src/product/upload.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller()
 export class UploadController {
   @Post('upload')
-  @UseInterceptors(FileInterceptor('image'))
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+      }),
+    }),
+  )
   uploadFile(@UploadedFile() file) {
     console.log(file);
   }
