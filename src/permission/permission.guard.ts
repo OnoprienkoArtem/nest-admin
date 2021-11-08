2cf6b422fd9dcedf6eda52213a30a8971ccdf2dc
OnoprienkoArtem <o.artem.e@gmail.com>
2021-11-08 22:39:34 +0200

    work with permission guard

--- src/permission/permission.guard.ts
+++ src/permission/permission.guard.ts
@@ -1,14 +1,31 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
+import { AuthService } from 'src/auth/auth.service';
+import { RoleService } from 'src/role/role.service';
+import { User } from 'src/user/models/user.entity';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class PermissionGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  constructor(
+    private reflector: Reflector,
+    private authService: AuthService,
+    private userService: UserService,
+    private roleService: RoleService,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
     const access = this.reflector.get<string>('access', context.getHandler());
+
+    if (!access) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const id = await this.authService.userId(request);
+    const user: User = await this.userService.findOne({ id }, ['role']);
+    const role = await this.roleService.findOne({ id: user.role.id });
+
     return true;
   }
 }
