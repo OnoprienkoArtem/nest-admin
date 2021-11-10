9a3bf879d80a591063cdee896f0f4e66fe4f28f5
OnoprienkoArtem <o.artem.e@gmail.com>
2021-11-10 23:02:35 +0200

    add permission decorator to order and role controller

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -14,6 +14,7 @@ import { Response } from 'express';
 import { Parser } from 'json2csv';
 import { OrderItem } from './models/order-item.entity';
 import { Order } from './models/order.entity';
+import { HasPermission } from 'src/permission/has-permission.decorator';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(AuthGuard)
@@ -22,11 +23,13 @@ export class OrderController {
   constructor(private orderService: OrderService) {}
 
   @Get('orders')
+  @HasPermission('orders')
   async all(@Query('page') page = 1) {
     return this.orderService.paginate(page, ['order_items']);
   }
 
   @Post('export')
+  @HasPermission('orders')
   async export(@Res() response: Response) {
     const parser = new Parser({
       fields: ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity'],
@@ -65,6 +68,7 @@ export class OrderController {
   }
 
   @Get('chart')
+  @HasPermission('orders')
   async chart() {
     return this.orderService.chart();
   }
--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -7,6 +7,7 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { HasPermission } from 'src/permission/has-permission.decorator';
 import { RoleService } from './role.service';
 
 @Controller('roles')
@@ -14,11 +15,13 @@ export class RoleController {
   constructor(private roleService: RoleService) {}
 
   @Get()
+  @HasPermission('roles')
   async all() {
     return this.roleService.all();
   }
 
   @Post()
+  @HasPermission('roles')
   async create(@Body('name') name: string, @Body('permissions') ids: number[]) {
     return this.roleService.create({
       name,
@@ -27,6 +30,7 @@ export class RoleController {
   }
 
   @Get(':id')
+  @HasPermission('roles')
   async get(@Param('id') id: number) {
     return this.roleService.findOne({ id }, ['permissions']);
   }
@@ -48,6 +52,7 @@ export class RoleController {
   }
 
   @Delete(':id')
+  @HasPermission('roles')
   async delete(@Param('id') id: number) {
     await this.roleService.delete(id);
   }
