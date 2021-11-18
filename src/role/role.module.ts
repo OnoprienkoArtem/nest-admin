69ee171c7dccd20b78b67178df220145a6576384
OnoprienkoArtem <o.artem.e@gmail.com>
2021-11-18 23:27:16 +0200

    export role service

--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -8,5 +8,6 @@ import { Role } from './role/role.entity';
   imports: [TypeOrmModule.forFeature([Role])],
   controllers: [RoleController],
   providers: [RoleService],
+  exports: [RoleService],
 })
 export class RoleModule {}
